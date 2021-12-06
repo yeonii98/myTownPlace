c81a00ec13eedcf043d424d7c2ca07c1af917d11
yeonii98 <ajy7424@naver.com>
2021-12-06 22:37:04 +0900

    지도 수정

--- src/main/java/com/ajy/mytownplace/kakao/dto/KakaoSearchLocalReq.java
+++ src/main/java/com/ajy/mytownplace/kakao/dto/KakaoSearchLocalReq.java
@@ -13,7 +13,7 @@ public class KakaoSearchLocalReq {
     //지역 검색 요청 변수에 대한 변수 생성
     private String query = ""; //검색을 원하는 문자열로서 UTF-8로 인코딩한다
     private int page = 1;
-    private int size = 7;
+    private int size = 9;
 
     public MultiValueMap<String, String> toMultiValueMap(){
         var map = new LinkedMultiValueMap<String, String>();
